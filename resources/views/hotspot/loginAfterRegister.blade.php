68ce22e15b7929cfe5117517692de24c9fb3ee8b
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-08-11 16:10:38 +0800

    perbaikan2

--- resources/views/hotspot/loginAfterRegister.blade.php
+++ resources/views/hotspot/loginAfterRegister.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body>
+<body onload="setTimeout(function() { document.login.submit() }, 100)">
 
 	<!-- HASH MD5 PASSWORD  -->
 	<script src="{{asset('login/js/md5.js')}}"></script>
@@ -15,7 +15,8 @@
 			var chap_id = "<?php echo($request['chap-id']) ?>";
 			var chap_challenge = "<?php echo($request['chap-challenge']) ?>";
 			var hash_pass = hexMD5(chap_id + document.login.password.value + chap_challenge);
-			console.log(hash_pash);
+			
+			console.log("Password Hashes : " + hash_pass);
 			document.login.password.value = hash_pass;
 			document.login.submit();
 			return false;
@@ -26,29 +27,17 @@
 
 				<form name="login" action="{{$request['link-login-only']}}" method="post" onsubmit="return doLogin()">
 				
-				@if($provider == 'facebook')
-					<input type="hidden" name="username" value="facebook_user">
-					<input type="hidden" name="password" value="facebook_user1234">
-				@else
-					<input type="hidden" name="username" value="google_user">
-					<input type="hidden" name="password" value="google_user1234">
-				@endif
+					@if($provider == 'facebook')
+						<input type="hidden" name="username" value="facebook_user">
+						<input type="hidden" name="password" value="facebook_user1234">
+					@else
+						<input type="hidden" name="username" value="google_user">
+						<input type="hidden" name="password" value="google_user1234">
+					@endif
 					
 
 				</form>
 
-				<script type="text/javascript">
-					window.onload=function(){
-
-						function submitform(){
-						//   alert('test');
-						  document.forms["login"].submit();
-						}
-
-						var auto = setTimeout(function(){ submitform(); }, 100);
-				
-					}
-				</script>
 
 </body>
 </html>

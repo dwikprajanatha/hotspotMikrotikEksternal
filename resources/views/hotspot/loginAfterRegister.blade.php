6e729f4d719524b37d75fdd865cdfdf4072cc68b
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-08-26 01:23:58 +0800

    perbaikan hotspot https2

--- resources/views/hotspot/loginAfterRegister.blade.php
+++ resources/views/hotspot/loginAfterRegister.blade.php
@@ -41,17 +41,8 @@
 		var password = "<?php echo($password) ?>";
 		var link_login = "<?php echo($request['link-login-only']) ?>";
 
-		// HEX PASSWORD
-		var chap_id = "<?php echo($request['chap-id']) ?>";
-		var chap_challenge = "<?php echo($request['chap-challenge']) ?>";
-		var hash_pass = hexMD5(chap_id + password + chap_challenge);
 
-		console.log("username : " + username);
-		console.log("password : " + password);
-		console.log("hash password : " + hash_pass);
-		console.log("link-login : " + link_login);
-
-		autoLogIn(username,hash_pass,link_login);
+		autoLogIn(username,password,link_login);
 
 	</script>
 	

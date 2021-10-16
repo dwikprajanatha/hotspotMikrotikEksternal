625464c29f997861c966a145e1ec1c726458abf0
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-10-16 15:31:58 +0800

    perbaikanBugTest9

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -383,14 +383,15 @@ class AdminController extends Controller
 
                 for ($i=0; $i < 7; $i++) {
 
-                    $penggunaanTotal = DB::connection('mysql_radius')->table('data_usage_by_period')
-                                            // ->select(DB::raw('(SUM(acctinputoctets)/1000/1000/1000) as GB_download')) 
-                                            // ->select(DB::raw('(SUM(acctoutputoctets)/1000/1000/1000) as GB_upload'))
-                                            ->whereNotNull('period_end')
-                                            ->whereDay('period_start', $date->format('Y-m-d'))
-                                            // ->groupBy(DB::raw('DAY(period_start)'))
-                                            ->get();
+                    // $penggunaanTotal = DB::connection('mysql_radius')->table('data_usage_by_period')
+                    //                         // ->select(DB::raw('(SUM(acctinputoctets)/1000/1000/1000) as GB_download')) 
+                    //                         // ->select(DB::raw('(SUM(acctoutputoctets)/1000/1000/1000) as GB_upload'))
+                    //                         ->whereNotNull('period_end')
+                    //                         ->whereDay('period_start', $date->format('Y-m-d'))
+                    //                         // ->groupBy(DB::raw('DAY(period_start)'))
+                    //                         ->get();
                     
+                    $penggunaanTotal = DB::DB::connection('mysql_radius')->table('data_usage_by_period')->get();
                     dd($penggunaanTotal);
 
                     array_push($arr_data, $penggunaanTotal->GB_total);

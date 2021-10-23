c1a530626dd0902fa12c80c121b50f4a98fe2597
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-10-23 11:35:28 +0800

    perbaikan Tampilan

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -23,7 +23,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{asset('admin/dist/img/logo-punggul.png')}}" height="60" width="60">
   </div>
   
 @if (Request::segment(1) != 'user')
@@ -146,9 +146,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="{{ route('admin.dashboard') }}" class="brand-link">
+      <img src="{{asset('admin/dist/img/logo-punggul.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin Hotspot Punggul</span>
     </a>
 
     <!-- Sidebar -->

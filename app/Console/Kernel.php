48574b32601c30bf68c1f9d4680764ca5b368ec0
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-10-28 15:06:16 +0800

    api get speed all user & limit session social user

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -27,55 +28,29 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function() {
 
-            $result = app('App\Http\Controllers\API\MikrotikController')->getSpeedAllConnectedUser();
+            try {
+                
+                $result = app('App\Http\Controllers\API\MikrotikController')->getSpeedAllConnectedUser();
 
-            $result = json_decode($result);
-
-            $result = $result->data->queues;
-
-            $arr_username = [];
-
-            DB::connecton('mysql')->transaction(function() use($arr_username, $result){
-
-                //ambil username
-                $users = DB::connection('mysql')->table('tb_average_speed')->select('username')->get();
-
-                foreach($users as $user){
-                    array_push($arr_username, $user->username);
-                }
-
-                for($i = 0; $i < count($result); $i++){
-
-                    if(!in_array($result[$i]->username, $arr_username)){
-
-                        DB::connection('mysql')->table('tb_average_speed')->insert([
-                            'username' => $result[$i]->username,
-                            'upload_speed' => $result[$i]->upload_speed,
-                            'download_speed' => $result[$i]->download_speed,
-                            'count' => 0,
-                            'created_at' => date('Y-m-d'),
-                        ]);
-
-                    } else {
-                        
-                        $user = DB::connection('mysql')->table('tb_average_speed')
-                                    ->select('upload_speed', 'download_speed', 'count', 'created_at')
-                                    ->where('username', $result[$i]->username)
-                                    ->whereRaw('MAX(created_at)')
-                                    ->first();
-                        
-                        if($user->created_at == date('Y-m-d')){
-
-                            DB::connection('mysql')->table('tb_average_speed')
-                                ->where('username', $result[$i]->username)
-                                ->update([
-                                    'upload_speed' => $users[$i]->upload_speed + $result[$i]->upload_speed,
-                                    'download_speed' => $users[$i]->download_speed + $result[$i]->download_speed,
-                                    'count' => $users[$i]->count + 1,
-                                ]);
-
-                        } else {
-
+                $result = json_decode($result);
+    
+                $result = $result->data->queues;
+    
+                $arr_username = [];
+    
+                DB::connecton('mysql')->transaction(function() use($arr_username, $result){
+    
+                    //ambil username
+                    $users = DB::connection('mysql')->table('tb_average_speed')->select('username')->get();
+    
+                    foreach($users as $user){
+                        array_push($arr_username, $user->username);
+                    }
+    
+                    for($i = 0; $i < count($result); $i++){
+    
+                        if(!in_array($result[$i]->username, $arr_username)){
+    
                             DB::connection('mysql')->table('tb_average_speed')->insert([
                                 'username' => $result[$i]->username,
                                 'upload_speed' => $result[$i]->upload_speed,
@@ -83,13 +58,48 @@ class Kernel extends ConsoleKernel
                                 'count' => 0,
                                 'created_at' => date('Y-m-d'),
                             ]);
-
+    
+                        } else {
+                            
+                            $user = DB::connection('mysql')->table('tb_average_speed')
+                                        ->select('upload_speed', 'download_speed', 'count', 'created_at')
+                                        ->where('username', $result[$i]->username)
+                                        ->whereRaw('MAX(created_at)')
+                                        ->first();
+                            
+                            if($user->created_at == date('Y-m-d')){
+    
+                                DB::connection('mysql')->table('tb_average_speed')
+                                    ->where('username', $result[$i]->username)
+                                    ->update([
+                                        'upload_speed' => $users[$i]->upload_speed + $result[$i]->upload_speed,
+                                        'download_speed' => $users[$i]->download_speed + $result[$i]->download_speed,
+                                        'count' => $users[$i]->count + 1,
+                                    ]);
+    
+                            } else {
+    
+                                DB::connection('mysql')->table('tb_average_speed')->insert([
+                                    'username' => $result[$i]->username,
+                                    'upload_speed' => $result[$i]->upload_speed,
+                                    'download_speed' => $result[$i]->download_speed,
+                                    'count' => 0,
+                                    'created_at' => date('Y-m-d'),
+                                ]);
+    
+                            }
+    
                         }
-
+    
                     }
+                });
 
-                }
-            });
+                Log::info("success running");
+            } catch (\Exception $e) {
+                Log::warning($e->getMessage());
+            }
+
+            
 
         })->everyMinute()->appendOutputTo(public_path('log/cron_log.log'));
     }

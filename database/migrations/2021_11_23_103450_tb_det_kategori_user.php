31843ef00a30c714fac4ec222eec66582e4aff10
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-11-23 21:26:40 +0800

    perbaikan migrasi tb_det_kategori_user

--- database/migrations/2021_11_23_103450_tb_det_kategori_user.php
+++ database/migrations/2021_11_23_103450_tb_det_kategori_user.php
@@ -15,6 +15,7 @@ class TbDetKategoriUser extends Migration
     {
         Schema::connection('mysql')->create('tb_det_kategori_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_kategori_user')->nullable()->constrained('tb_kategori_user');
             $table->foreignId('id_user_social')->nullable()->constrained('tb_user_social');
             $table->foreignId('id_user_hotspot')->nullable()->constrained('tb_user_hotspot');
         });

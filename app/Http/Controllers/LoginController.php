25bcb6eb47a3958311d9b2cb83baf696493b9199
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-12-02 17:20:23 +0800

    perbaiki status7

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,12 +32,12 @@ class LoginController extends Controller
         $date = new DateTime();
 
         $data_usage = DB::connection('mysql_radius')->table('radacct')
-                        ->select('username', 'acctstarttime', 'acctstoptime', DB::raw('(SUM(acctinputoctets)/1000/1000/1000) as acctinputoctets'), DB::raw('(SUM(acctoutputoctets)/1000/1000/1000) as acctoutputoctets'))
-                        ->where('username', $request->username)
-                        // ->whereDate('acctstarttime', '<=', $date->format('Y-m-d'))
-                        ->whereDate('acctstarttime', '>=', $date->modify('-6 day')->format('Y-m-d'))
-                        // ->groupBy(DB::raw('DATE(acctstarttime)'))
-                        ->get();
+                                ->select('username', 'acctstarttime', 'acctstoptime', DB::raw('(SUM(acctinputoctets)/1000/1000/1000) as acctinputoctets'), DB::raw('(SUM(acctoutputoctets)/1000/1000/1000) as acctoutputoctets'))
+                                ->where('username', $request->username)
+                                // ->whereDate('acctstarttime', '<=', $date->format('Y-m-d'))
+                                ->whereDate('acctstarttime', '>=', $date->modify('-6 day')->format('Y-m-d'))
+                                ->groupBy('username')
+                                ->get();
 
         dd($data_usage);
                 

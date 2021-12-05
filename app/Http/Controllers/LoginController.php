20bff8dee660cc09b9961bc3d0c11e36eab16e55
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-12-06 02:16:05 +0800

    perbaikan test login2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -57,7 +57,7 @@ class LoginController extends Controller
             array_push($arr_pengumuman, $p);
         }
 
-        dd((object)$arr_pengumuman);
+        // dd((object)$arr_pengumuman);
 
         return view('hotspot.logintest', ['request' => $request->all(), 'pengumuman' => (object)$arr_pengumuman]);
     }

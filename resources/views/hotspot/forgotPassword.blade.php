6ee21e39ed00b6011251d0c6180dcd0a3be54b97
Dwik Prajanatha <dwikprajanatha@gmail.com>
2021-12-06 21:22:12 +0800

    perbaikan forgot password4

--- resources/views/hotspot/forgotPassword.blade.php
+++ resources/views/hotspot/forgotPassword.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Buat Akun Baru</title>
+	<title>Forgot Password</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
